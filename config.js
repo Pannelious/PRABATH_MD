bac171341267275795604a15637cff7c2b6423ad
Pannelious <101747535+Pannelious@users.noreply.github.com>
2024-05-18 17:21:29 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,29 +3,29 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
-global.mongodb = process.env.MONGODB_URI || "Enter-MongoURI-HERE"
+global.owner = process.env.OWNER_NUMBER.split("263732872101,+263 71 858 0615")
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Pannelious:masterpeace@atlascluster.ykfgwfj.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'sam@sec.live'
-global.github = 'https://github.com/prabathLK/PRABATH-MD'
+global.github = 'https://github.com/pannelious/PRABATH-MD'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
-global.devs = '94762280384';
+global.devs = '263732872101';
 global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+94762280384
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/H062oEI.jpeg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/a87f18815f9b61b7090bb.jpg'
 module.exports = {
   OPENAI_API_KEY:process.env.OPENAI_API_KEY || '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',
   botname: process.env.BOT_NAME || 'PRABATH-MD',
   dl_limit: process.env.DL_SIZE || '300',
   art: process.env.AUTO_RESTART || 'true',
   sudo: process.env.SUDO || '94762280384',
-  alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg',
-  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY PRABATH-MD* 🎊',
-  ownername:process.env.OWNER_NAME || 'PRABATH',
-  sessionName: process.env.SESSION_ID || 'PUT-HERE',
+  alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/a87f18815f9b61b7090bb.jpg',
+  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY MASTERPEACE-MD* 🎊',
+  ownername:process.env.OWNER_NAME || 'MASTERPEACE',
+  sessionName: process.env.SESSION_ID || 'PRABATH-MD~9iMERQBa#Re6shOgk7W1ObjjaLXwfQAeqvpOBfUULf5-Fp8b3Iy4',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
-  packname: process.env.PACK_INFO.split(";")[1] || 'Name',
+  packname: process.env.PACK_INFO.split(";")[1] || 'MASTERPEACE',
   autoreaction: process.env.AUTO_REACTION || 'off',
   antibadword : process.env.ANTI_BAD_WORD || 'fuck,huth,fucking,huththa,huththo,hukanna,fucked,pakaya,pako,ponnaya,ponnayo,kariya,kariyo,huththaa,hutta',
   alwaysonline: process.env.ALWAYS_ONLINE || 'true',
@@ -33,12 +33,12 @@ module.exports = {
   readmessage: process.env.READ_MESSAGE || true,
   HANDLERS: process.env.PREFIX || ['.'],
   warncount : process.env.WARN_COUNT || 3,
-  disablepm: process.env.DISABLE_PM || "flase",
+  disablepm: process.env.DISABLE_PM || "true",
   levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
   antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
   antilinkaction: process.env.ANTILINK_ACTION || 'remove',
-  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || 'put your herokuapp name',
-  HEROKU_API_KEY: process.env.HEROKU_API_KEY || 'put your heroku_api_key',
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || 'buru',
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || 'HRKU-468109f7-72de-4380-9fd0-16e9e48dca9c',
   BRANCH: 'main',
   ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || '',
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'put-key-here',
